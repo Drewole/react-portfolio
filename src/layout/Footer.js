41723776c604ccc3a43abeb264bf8f44a68588b5
Drew Olsen <olsenj.andrew@gmail.com>
2021-05-13 01:31:36 -0500

    More frameworking

---
+++ src/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Footer = () => {
+	return (
+		<footer>
+			<p class="cheatcode">↑ ↑ ↓ ↓ ← → ← → A B B A</p>
+			<p class="copyright">&copy; Drew Olsen, LLC</p>
+		</footer>
+	);
+};
